c054e6b3c4e09dfcee5730648d6b16d7aa5712eb
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-08-15 09:52:43 +1000

    Testing
    I was going through and testing my cleaning code and it has worked except for one line wear i need to remove the letter graded scores, next i need to figure out how to average scores with the same id

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
 # Setup of pandas and matplotlib
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Making dataframe, cleaning and analysing
-rotten_reviews_df = pd.read_csv('data/rotten_tomatoes_movie_reviews.csv')
-rotten_reviews_df.drop(columns=['criticName', 'reviewUrl', 'publicatioName'], inplace=True)
-rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['isTopCritic'] == 'True']
-rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['originalScore'] =! '']
-rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['originalScore'] =! float]
