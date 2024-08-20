89e7a6a976c3898ddcc0cd896bdad05fcf358c5b
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-08-20 11:11:12 +1000

    Sad float time
    I was trying to convert the original score colomn into a float but it doesnt work and I am sad

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Setup of pandas and matplotlib
+import pandas as pd
+
+
+# Making dataframe, cleaning and analysing
+#rotten_reviews_df = pd.read_csv('data/rotten_tomatoes_movie_reviews.csv', on_bad_lines='warn' )
+#rotten_reviews_df.drop(columns=['criticName', 'reviewUrl', 'publicatioName', 'reviewText'], inplace=True)
+#rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['isTopCritic'] == True]
+#rotten_reviews_df.dropna(inplace=True)
+#rotten_reviews_df = rotten_reviews_df.query(f'originalScore.str.contains("/")', engine='python')
+###rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['originalScore'] * 100]
+
+#rotten_reviews_df['originalScore'].duplicated.mean() #Try to average duplicate ids
+
+##print(rotten_reviews_df)
+
+##rotten_reviews_df.to_csv('data/cleaned data.csv', index=False)
+rotten_reviews_df = pd.read_csv('data/cleaned data.csv', on_bad_lines='warn' )
+#rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains(".")', engine='python')
+#rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].astype(int)
+print(rotten_reviews_df)
