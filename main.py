bf5b19919f36b24647823b39df328cb5fbf8ae54
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-08-21 09:01:30 +1000

    pain
    i removed unessicery code and the missing data point changed places and I dont know why but i have to find it again

--- main.py
+++ main.py
@@ -3,19 +3,11 @@ import pandas as pd
 
 
 # Making dataframe, cleaning and analysing
-#rotten_reviews_df = pd.read_csv('data/rotten_tomatoes_movie_reviews.csv', on_bad_lines='warn' )
-#rotten_reviews_df.drop(columns=['criticName', 'reviewUrl', 'publicatioName', 'reviewText'], inplace=True)
-#rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['isTopCritic'] == True]
-#rotten_reviews_df.dropna(inplace=True)
-#rotten_reviews_df = rotten_reviews_df.query(f'originalScore.str.contains("/")', engine='python')
-###rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['originalScore'] * 100]
-
-#rotten_reviews_df['originalScore'].duplicated.mean() #Try to average duplicate ids
-
-##print(rotten_reviews_df)
-
-##rotten_reviews_df.to_csv('data/cleaned data.csv', index=False)
-rotten_reviews_df = pd.read_csv('data/cleaned data.csv', on_bad_lines='warn' )
+rotten_reviews_df = pd.read_csv('data/rotten_tomatoes_movie_reviews.csv', on_bad_lines='warn' )
+rotten_reviews_df.drop(columns=['criticName', 'reviewUrl', 'publicatioName', 'reviewText'], inplace=True)
+rotten_reviews_df = rotten_reviews_df[rotten_reviews_df['isTopCritic'] == True]
+rotten_reviews_df.dropna(inplace=True)
+rotten_reviews_df = rotten_reviews_df.query(f'originalScore.str.contains("/")', engine='python')
 rotten_reviews_df.drop(columns=['reviewId'], inplace=True)
 
 rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains(" ")', engine='python') #clean
@@ -25,15 +17,12 @@ rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("f"
 rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("a")', engine='python') #clean
 rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace("'", '')
 rotten_reviews_df.drop(rotten_reviews_df.index[192242], inplace=True)
-rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace(' ', '')
+
+#---------------------------------------------------------------------------------------------------------------------------------------
 
 rotten_reviews_df[['score', 'diviser']] = rotten_reviews_df['originalScore'].str.split('/', n=1, expand=True)
-
 rotten_reviews_df.dropna(inplace=True)
-
-
-rotten_reviews_df.drop(rotten_reviews_df.index[95362:95363], inplace=True)
-
+rotten_reviews_df.drop(rotten_reviews_df.index[95300:95500], inplace=True)
 
 rotten_reviews_df['score'] = rotten_reviews_df['score'].astype(float)
 rotten_reviews_df['diviser'] = rotten_reviews_df['diviser'].astype(float)
@@ -41,16 +30,13 @@ rotten_reviews_df['diviser'] = rotten_reviews_df['diviser'].astype(float)
 rotten_reviews_df['percentScore'] = rotten_reviews_df['score']/rotten_reviews_df['diviser']*100
 rotten_reviews_df.drop(columns=['originalScore', 'score', 'diviser'], inplace=True)
 
-#print(rotten_reviews_df)
-
 #---------------------------------------------------------------------------------------------------------------------------------------
 
 averagedScore_df = rotten_reviews_df.drop(columns=['creationDate','isTopCritic','reviewState','scoreSentiment'])
 averagedScore_df = averagedScore_df.groupby(['id']).mean()
 averagedScore_df = averagedScore_df.reset_index()
 averagedScore_df = averagedScore_df.round(2)
-#print(averagedScore_df)
-#averagedScore_df.to_csv('data/average.csv', index=False)
+
 #---------------------------------------------------------------------------------------------------------------------------------------
 
 id_df = rotten_reviews_df.drop(columns=['isTopCritic','reviewState','scoreSentiment','percentScore'])
@@ -58,10 +44,6 @@ id_df['id'] = id_df['id'].drop_duplicates()
 id_df.dropna(inplace=True)
 id_df.sort_values(by=['id'], inplace=True)
 id_df = id_df.reset_index()
-
-#print(id_df)
-
-id_df.to_csv('data/ids.csv', index=False)
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 
