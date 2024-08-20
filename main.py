61edc587b706a5f2a64ec2ee6141b640725891d0
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-08-20 20:18:51 +1000

    sir i did it
    i finished almost all of my code, I believe all I have left is the data visuilisation and making it look nice. firstly I split the original score column into a score and deviser which I could use to get a percentage score, but in trying to convert them too floats I found alot of mistakes in the data that I had to fix including a missing value that wouldn't go away and I had to manually find and delete as I couldn't use ctrl + f on nothing, and it didn't count as a missing value so the nan thing didn't work. I then made a new dataframe to average all of the scores which took half an hour to try to fix as I had forgot a bracket in one of the lines. I then made a id list with all of the creation dates and dropped all duplicate columns gibing only the first creation date in the csv file for each id. I then had to make a new dataframe to put the data in which presented problems with me having to reset the index for the id_df dates to line up properly. now I have most of what I need for visualisation.

--- main.py
+++ main.py
@@ -16,11 +16,62 @@ import pandas as pd
 
 ##rotten_reviews_df.to_csv('data/cleaned data.csv', index=False)
 rotten_reviews_df = pd.read_csv('data/cleaned data.csv', on_bad_lines='warn' )
-rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace('/5', '')
-rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace('/4', '')
+rotten_reviews_df.drop(columns=['reviewId'], inplace=True)
+
+rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains(" ")', engine='python') #clean
+rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("`")', engine='python') #clean
+rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("/4/")', engine='python') #clean
+rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("f")', engine='python') #clean
+rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains("a")', engine='python') #clean
+rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace("'", '')
+rotten_reviews_df.drop(rotten_reviews_df.index[192242], inplace=True)
+rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].str.replace(' ', '')
+
+rotten_reviews_df[['score', 'diviser']] = rotten_reviews_df['originalScore'].str.split('/', n=1, expand=True)
+
+rotten_reviews_df.dropna(inplace=True)
 
 
+rotten_reviews_df.drop(rotten_reviews_df.index[95362:95363], inplace=True)
 
-#rotten_reviews_df = rotten_reviews_df.query(f'not originalScore.str.contains(".")', engine='python')
-#rotten_reviews_df['originalScore'] = rotten_reviews_df['originalScore'].astype(float)
-print(rotten_reviews_df)
+
+rotten_reviews_df['score'] = rotten_reviews_df['score'].astype(float)
+rotten_reviews_df['diviser'] = rotten_reviews_df['diviser'].astype(float)
+
+rotten_reviews_df['percentScore'] = rotten_reviews_df['score']/rotten_reviews_df['diviser']*100
+rotten_reviews_df.drop(columns=['originalScore', 'score', 'diviser'], inplace=True)
+
+#print(rotten_reviews_df)
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+averagedScore_df = rotten_reviews_df.drop(columns=['creationDate','isTopCritic','reviewState','scoreSentiment'])
+averagedScore_df = averagedScore_df.groupby(['id']).mean()
+averagedScore_df = averagedScore_df.reset_index()
+averagedScore_df = averagedScore_df.round(2)
+#print(averagedScore_df)
+#averagedScore_df.to_csv('data/average.csv', index=False)
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+id_df = rotten_reviews_df.drop(columns=['isTopCritic','reviewState','scoreSentiment','percentScore'])
+id_df['id'] = id_df['id'].drop_duplicates()
+id_df.dropna(inplace=True)
+id_df.sort_values(by=['id'], inplace=True)
+id_df = id_df.reset_index()
+
+#print(id_df)
+
+id_df.to_csv('data/ids.csv', index=False)
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+final_data_df = pd.DataFrame(columns=['id', 'score', 'date'])
+
+final_data_df['date'] = id_df['creationDate']
+final_data_df['id'] = averagedScore_df['id']
+final_data_df['score'] = averagedScore_df['percentScore']
+
+
+print(final_data_df)
+
+final_data_df.to_csv('data/final.csv', index=False)
